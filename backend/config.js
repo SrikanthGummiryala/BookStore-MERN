c3277cd32fbc59687b842eb64f74582e4091f123
Gummiryala Srikanth <gummiryalasrikanth369@gmail.com>
2025-04-02 16:37:35 +0530

    BookStore-MERN

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 4000;
+export const mongoDBURL =
+  "mongodb+srv://root:root@bookstore-mern.kmbunnf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN";
